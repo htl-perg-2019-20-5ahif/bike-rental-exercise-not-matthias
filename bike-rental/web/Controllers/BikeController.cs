9e30c31f6f230645bbfd9ab3b71b828dba956c60
not-matthias <26800596+not-matthias@users.noreply.github.com>
2019-11-16 13:13:02 +0100

    Clean project setup

---
+++ bike-rental/web/Controllers/BikeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BikeController : ControllerBase
+    {
+        private readonly ILogger<BikeController> _logger;
+
+        public BikeController(ILogger<BikeController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
