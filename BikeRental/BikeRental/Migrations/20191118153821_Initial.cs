082e39c7dad463b5bf6c4288a78b0e00023f06df
not-matthias <26800596+not-matthias@users.noreply.github.com>
2019-11-18 16:38:42 +0100

    Added migration

---
+++ BikeRental/BikeRental/Migrations/20191118153821_Initial.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BikeRental.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bikes",
+                columns: table => new
+                {
+                    BikeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Brand = table.Column<string>(maxLength: 25, nullable: false),
+                    PurchaseDate = table.Column<DateTime>(nullable: false),
+                    Notes = table.Column<string>(maxLength: 1000, nullable: true),
+                    LastService = table.Column<DateTime>(nullable: true),
+                    RentalPriceFirstHour = table.Column<decimal>(nullable: false),
+                    RentalPriceAdditionalHours = table.Column<decimal>(nullable: false),
+                    BikeCategory = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bikes", x => x.BikeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Gender = table.Column<int>(nullable: false),
+                    FirstName = table.Column<string>(maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(maxLength: 75, nullable: false),
+                    Birthday = table.Column<DateTime>(nullable: false),
+                    Street = table.Column<string>(maxLength: 75, nullable: false),
+                    HouseNumber = table.Column<string>(maxLength: 10, nullable: true),
+                    ZipCode = table.Column<string>(maxLength: 10, nullable: false),
+                    Town = table.Column<string>(maxLength: 75, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rentals",
+                columns: table => new
+                {
+                    RentalId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RentalBegin = table.Column<DateTime>(nullable: false),
+                    RentalEnd = table.Column<DateTime>(nullable: true),
+                    TotalRentalCosts = table.Column<decimal>(nullable: true),
+                    Paid = table.Column<bool>(nullable: false),
+                    RenterId = table.Column<int>(nullable: false),
+                    BikeId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rentals", x => x.RentalId);
+                    table.ForeignKey(
+                        name: "FK_Rentals_Bikes_BikeId",
+                        column: x => x.BikeId,
+                        principalTable: "Bikes",
+                        principalColumn: "BikeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Rentals_Customers_RenterId",
+                        column: x => x.RenterId,
+                        principalTable: "Customers",
+                        principalColumn: "CustomerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rentals_BikeId",
+                table: "Rentals",
+                column: "BikeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rentals_RenterId",
+                table: "Rentals",
+                column: "RenterId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Rentals");
+
+            migrationBuilder.DropTable(
+                name: "Bikes");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
